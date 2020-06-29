a88bb8e43b9bf5d369e8a992c8ed48c9b0524a23
yrd <yash.deorah@somaiya.edu>
2020-06-29 15:55:34 +0530

    debug false

--- bloombox/settings.py
+++ bloombox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cn0(v3%!x#f=(=h)h(ng45f!twtcrk=$f)k%fe3x5nr51(3j4h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bloomboxkjsce.herokuapp.com','127.0.0.1','www.bloomboxkjsce.com']
 
