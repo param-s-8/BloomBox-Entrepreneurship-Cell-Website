970de7b9e2dd888b9fad162fc9d437741e57ef1f
passedonwisdom <yash.deorah@somaiya.edu>
2021-02-12 12:07:05 +0530

    enspire and 2 cc added

--- bb_app/views.py
+++ bb_app/views.py
@@ -12,7 +12,7 @@ def index(request):
 
 def campus(request):
     companies=Campus.objects.all().order_by("name")
-    active_companies=["Nurturing Lives","Synergy","LAUNCHiT","&Evolve","Learnation","Notebook"]
+    active_companies=["Nurturing Lives","Synergy","LAUNCHiT","&Evolve","Learnation","Notebook","Pasken","Studbud"]
     context={'companies':companies,'active_companies':active_companies}
     return render(request,"campus-companies.html",context)
 
